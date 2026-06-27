928ab0c0b08d0ba2d213719da735105b24c60df5
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-06-27 09:33:52 +0600

    uspauded

--- xubitar-pre-order-manager-client/src/components/PreOrderListPage/PreOrderTypes.ts
+++
@@ -1,8 +0,0 @@
-export const tabs = ["All", "Active", "Inactive"] as const;
-
-export type Tab = (typeof tabs)[number];
-
-
-
-export type SortField = "name" | "createdAt" | "startsAt" | "endsAt";
-export type SortDirection = "asc" | "desc";
